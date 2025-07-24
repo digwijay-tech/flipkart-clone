c4de6c101e723417742ec167893deb22999261f2
digwijay-tech <digwijay2@gmail.com>
2025-07-24 16:23:57 +0530

    feat: implement horizontal scrolling functionality in ProductsDisplay component; add navigation buttons for product carousel

--- src/component/ProductDisplay.tsx/ProductsDisplay.tsx
+++ src/component/ProductDisplay.tsx/ProductsDisplay.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ProductCard from "./ProductCard";
 import axios from "axios";
 type ProductProps = {
@@ -8,6 +8,55 @@ type ProductProps = {
 const ProductsDisplay = ({ categoryname, style }: ProductProps) => {
   const [productTitle, setProductTitle] = useState();
   const [productDetails, setProductDetails] = useState([]);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [translate, setTranslate] =useState(0)
+  const [canScrollMore, setCanScrollMore]=useState(true)
+  
+  // const [buttonVisiblity, setButtonVisiblity]= useState(true)
+  // useEffect(()=>{
+  //   const wrapper = wrapperRef.current
+  //   const scroll = scrollRef.current
+  //   if(!wrapper || !scroll) return;
+    
+  //   const wrapperWidth = wrapper.offsetWidth
+  //   const scrollWidth = scroll.scrollWidth
+  //     setButtonVisiblity(visibility =>wrapperWidth >= scrollWidth ? !visibility : visibility)
+  // },[translate])
+
+  const handleNext = () => {
+    const wrapper = wrapperRef.current
+    const scroll = scrollRef.current
+    if(!wrapper || !scroll) return;
+    
+    const wrapperWidth = wrapper.offsetWidth
+    const scrollWidth = scroll.scrollWidth
+    const hiddenWidth= scrollWidth-wrapperWidth
+
+    if(hiddenWidth > 0){
+      const moveBy = Math.min(wrapperWidth , hiddenWidth)
+      setTranslate(prev => prev-moveBy)
+
+      setCanScrollMore(!canScrollMore)
+    }
+    
+  };
+    const handlePrev = () => {
+    const wrapper = wrapperRef.current
+    console.log("previous")
+     setCanScrollMore(!canScrollMore)
+    if(!wrapper ) return;
+    
+    const wrapperWidth = wrapper.offsetWidth
+    
+    const canscroll = Math.abs(translate)>0
+
+    if(canscroll){
+      const moveBy = Math.min(wrapperWidth , Math.abs(translate))
+      setTranslate(prev => prev+moveBy)
+    }
+    
+  };
   useEffect(() => {
     axios
       .get("/carouse.json")
@@ -26,14 +75,26 @@ const ProductsDisplay = ({ categoryname, style }: ProductProps) => {
             {productTitle}
           </div>
         </div>
-        <div className="flex flex-col relative">
-          <div className="flex overflow-x-scroll overflow-y-hidden scroll-smooth snap-mandatory p-1.5 [scrollbar-width:none] pb-3">
+        <div
+          ref={wrapperRef}
+          className="flex flex-col relative min-[1190px]:overflow-hidden "
+        >
+          <div
+            ref={scrollRef}
+            className="flex max-[1190px]:overflow-x-scroll scroll-smooth  w-full transition-[transform_.4s_ease-in-out] snap-start p-1.5 [scrollbar-width:none] pb-3"
+            style={{
+              transform: `translateX(${translate}px)`,
+            }}
+          >
             {productDetails.map((productdetail, index) => (
               <ProductCard product={productdetail} key={index} />
             ))}
           </div>
-          <button className=" hidden absolute   bottom-0 top-20 right-0 min-[1190px]:flex h-[88px] w-10 shadow-[0px_1px_4px_rgba(0,0,0,.12)] rounded-[4px_0px_0px_4px] bg-white">
+          <button className={` hidden absolute cursor-pointer  bottom-0 top-20 right-0  h-[88px] w-10 shadow-[0px_1px_4px_rgba(0,0,0,.12)] rounded-[4px_0px_0px_4px] bg-white ${canScrollMore? "min-[1190px]:flex":"" }`} onClick={handleNext} >
             <span className="absolute right-[16px] top-[50%] transform-[scale(1)_translateY(-50%)] before: [content:''] before:relative before:border-[#878787] before:border-[2px_2px_0px_0px] before:inline-block before:h-[6px] before:w-[6px] before:transform-[rotate(45deg)] "></span>
+          </button>
+          <button className={` hidden absolute cursor-pointer  bottom-0 top-20 left-0  h-[88px] w-10 shadow-[0px_1px_4px_rgba(0,0,0,.12)] rounded-[4px_0px_0px_4px] bg-white ${!canScrollMore? "min-[1190px]:flex":"" } `} onClick={handlePrev}>
+            <span className="absolute right-[16px] top-[50%] transform-[scale(1)_translateY(-50%)] before: [content:''] before:relative before:border-[#878787] before:border-[2px_2px_0px_0px] before:inline-block before:h-[6px] before:w-[6px] before:transform-[rotate(-135deg)] "></span>
           </button>
         </div>
       </div>
